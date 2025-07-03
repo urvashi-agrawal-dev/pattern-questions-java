6d11a32177c0164fe6c07f0c988d6c96236c2084
Urvashi Agrawal <158180920+urvashi-agrawal-dev@users.noreply.github.com>
2025-07-03 10:02:04 +0000

    vb

--- InvertedRightAngledTraingle.java
+++ InvertedRightAngledTraingle.java
@@ -1,7 +1,7 @@
 // File: invertedrightangledtraingle.java
 public class InvertedRightAngledTraingle {  // Class name matches filename
     public static void main(String[] args) {
-        int rows = 5;
+        int rows = 4;
         for (int i = rows; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
