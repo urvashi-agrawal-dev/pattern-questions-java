0138c8cabd3de084d5f46b98e0923ca1135a3ad9
Urvashi Agrawal <158180920+urvashi-agrawal-dev@users.noreply.github.com>
2025-07-04 05:41:50 +0000

    ff

---
+++ nosleft.java
@@ -0,0 +1,14 @@
+class nosleft{
+    public static void main(String[] args){
+        int n=5;
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            // for(int j=1;j<=i;j++){
+            //     System.out.print("1");
+            // }
+            System.out.println();
+        }
+    }
+}   
